ce5ef69040836e1414766ff43dedf142bbdccc6b
Tim Johansson <tim.den.vilde@gmail.com>
2023-07-19 02:31:47 +0200

    some fixes

--- sudoku-helper.py
+++ sudoku-helper.py
@@ -4,6 +4,7 @@ import argparse
 import functools
 from collections import defaultdict
 from contextlib import suppress
+from contextvars import ContextVar
 from functools import reduce, partial
 from functools import total_ordering
 import itertools
@@ -49,33 +50,39 @@ class SudokuSet(frozenset):
         return self.__hash__() > other.__hash__()
 
 
-colors = {str(k): f"bold grey3 on {v}" for k, v in zip(count(1), [
-    "#005F73",
-    "#0A9396",
-    "#94D2BD",
-    "#E9D8A6",
-    "#EE9B00",
-    "#CA6702",
-    "#BB3E03",
-    "#AE2012",
-    "#9B2226",
-])}
+colors = defaultdict(
+    lambda: "bold grey3 on #dd11ee", {
+        str(k): f"bold grey3 on {v}"
+        for k, v in zip(count(1),
+                        ["#005F73",
+                         "#0A9396",
+                         "#94D2BD",
+                         "#E9D8A6",
+                         "#EE9B00",
+                         "#CA6702",
+                         "#BB3E03",
+                         "#AE2012",
+                         "#9B2226"])
+    })
 
 
 def sub_color(text):
     return re.sub(r"\s(\d)\s", lambda s: f"[{colors[s[1]]}]{s[0]}[/]", text)
 
 
+MaskType = tuple[tuple[int | None, int | None], list[int]]
+
+
 class Args(argparse.Namespace):
     subparser: str | None
     sums: list[int]
     num_digits: list[int]
-    digit_set: list[int]
-    include_digits: list[tuple[tuple[int, int], list[int]]]
-    exclude_digits: list[tuple[tuple[int, int], list[int]]]
+    digit_set: MaskType
+    include_digits: list[MaskType]
+    exclude_digits: list[MaskType]
     num_odd: int | None
     num_even: int | None
-    mask: list[tuple[tuple[int, int], list[int]]]
+    mask: MaskType
     nop: bool
 
 
@@ -99,11 +106,13 @@ def merge(a, b) -> Alternatives:
 def exclusive(a: Alternatives, b: Alternatives, args: Args) -> Alternatives:
     """keep alternatives where the joint set are all unique"""
     rows = list(merge(a, b))
+    if not rows:
+        return iter(())
     masks = create_index_masks(args.mask, len(rows[0]))
 
     def sets_disjoint(row):
         sets = build_sets_from_index_mask(masks, row)
-        return bool(reduce(methodoperator("isdisjoint"), sets))
+        return all(x.isdisjoint(y) for x, y in combinations(sets, 2))
 
     return (x for x in rows if sets_disjoint(x))
 
@@ -116,7 +125,7 @@ def same_sum(a: Alternatives, b: Alternatives) -> Alternatives:
 
 
 def get_number_mask(args_mask) -> Iterator[int]:
-    return chain.from_iterable(m[1] for m in chain(args_mask))
+    return iter(chain(args_mask[1]))
 
 
 @default_nop
@@ -141,7 +150,7 @@ def permute(a: Alternatives, b: Alternatives, args: Args) -> Alternatives:
 
 def create_index_masks(args_mask, length):
     num_sets = list(range(length))
-    if not args_mask:
+    if not args_mask[1]:
         mask = (num_sets[:-1], num_sets[-1:])
     else:
         mask_dict = defaultdict(list)
@@ -176,11 +185,19 @@ def overlap(a: Alternatives, b: Alternatives, args: Args) -> Alternatives:
     1st and 3rd column doesn't have an overlap with the 2nd
     """
     rows = list(merge(a, b))
+    if not rows:
+        return iter(())
     masks = create_index_masks(args.mask, len(rows[0]))
+    min_overlap, max_overlap = args.mask[0]
+    if not min_overlap:
+        min_overlap = 1 if not max_overlap else max_overlap
+    if not max_overlap:
+        max_overlap = len(args.digit_set[1])
 
     def sets_overlap(row):
         sets = build_sets_from_index_mask(masks, row)
-        return bool(reduce(methodoperator("intersection"), sets))
+        return all(min_overlap <= len(x.intersection(y)) <= max_overlap
+                   for x, y in combinations(sets, 2))
 
     return (x for x in rows if sets_overlap(x))
 
@@ -202,7 +219,10 @@ operators: dict[str, Callable[[Alternatives, Alternatives], Alternatives]] = {
 
 class SetAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
-        setattr(namespace, self.dest, [*getattr(namespace, self.dest, []), *values])
+        if self.nargs:
+            setattr(namespace, self.dest, [*getattr(namespace, self.dest, []), *values])
+        else:
+            setattr(namespace, self.dest, values)
 
 
 def set_type(string: str):
@@ -212,16 +232,11 @@ def set_type(string: str):
             f"{string!r} invalid syntax, must be [d-d:]XXXX "
             f"ex. 1-2:12345 => include 1 or 2 of set {{1,2,3,4,5}}")
     digits = [int(x) if x.isdigit() else -1 for x in match[3]]
-    start = match[1]
-    end = match[2]
 
-    if not start and not end:
-        start = end = len(digits)
-    if not start:
-        start = 0
-    if not end:
-        end = len(digits)
-    return (int(start), int(end)), digits
+    start = int(match[1]) if match[1] else None
+    end = int(match[2]) if match[2] else None
+
+    return (start, end), digits
 
 
 notes = []
@@ -232,20 +247,24 @@ def defer_note(depth, msg=None):
         atexit.register(defer_note, -1)
 
     if depth >= 0:
-        notes.append((depth, f"\n  [red]Note: [/]{msg}\n"))
+        notes.append((depth, f"  [red]Note: [/]{msg}\n"))
         return
 
     d, note = notes.pop()
+    print("\n ", "- " * 20, sep="")
     while True:
         print(note)
         with suppress(IndexError):
             temp, note = notes.pop()
-            if temp < d:
-                return
+            if temp == d:
+                continue
         return
 
 
-def main(prev_alternatives: Alternatives = None, raw_args: list[str] = None, depth=0) -> Alternatives:
+depth_var: ContextVar[int] = ContextVar("depth", default=0)
+
+
+def main(prev_alternatives: Alternatives = [], raw_args: list[str] = None) -> Alternatives:
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--sums", type=int, nargs="*", metavar="SUM",
                         help="sums to match of given digits (DEFAULT: %(default)s)")
@@ -254,8 +273,8 @@ def main(prev_alternatives: Alternatives = None, raw_args: list[str] = None, dep
                         help="number of digits to match (DEFAULT: %(default)s)")
     parser.add_argument("--nop", action=argparse.BooleanOptionalAction, default=False,
                         help="nop adding digits. default depends on command")
-    parser.add_argument("-g", "--digit-set", type=int, nargs="*", metavar="DIGIT",
-                        default=[1, 2, 3, 4, 5, 6, 7, 8, 9],
+    parser.add_argument("-g", "--digit-set", type=set_type, metavar="DIGIT",
+                        action=SetAction, default=((None, None), [1, 2, 3, 4, 5, 6, 7, 8, 9]),
                         help="starting options (DEFAULT: %(default)s)")
     parser.add_argument("-i", "--include-digits", type=set_type, nargs="*", metavar="DIGIT",
                         action=SetAction, default=[],
@@ -269,7 +288,7 @@ def main(prev_alternatives: Alternatives = None, raw_args: list[str] = None, dep
                         help="the minimum number of odd number to match")
     parser.add_argument("-e", "--num-even", type=int, metavar="NUM",
                         help="the minimum number of even number to match")
-    parser.add_argument("-m", "--mask", type=set_type, nargs="*", action=SetAction, default=[],
+    parser.add_argument("-m", "--mask", type=set_type, action=SetAction, default=((None, None), []),
                         help="a mask for various commands (DEFAULT: %(default)s)")
 
     parser.add_argument("--", dest="bork", choices=operators.keys(), nargs="*",
@@ -287,7 +306,7 @@ def main(prev_alternatives: Alternatives = None, raw_args: list[str] = None, dep
             if (sep := raw_args.pop(0)) != "--":
                 parser.error(f"wrong separator: {sep!r}")
             if not raw_args:
-                defer_note(depth - 1, "missing chain operator")
+                defer_note(depth_var.get() - 1, "missing chain operator")
                 return prev_alternatives
             if (op_func := operators.get(op := raw_args.pop(0).upper(), None)) is None:
                 parser.error(f"invalid chain operator: {op!r}")
@@ -300,8 +319,7 @@ def main(prev_alternatives: Alternatives = None, raw_args: list[str] = None, dep
     rest: list[str]
     args, rest = parser.parse_known_args(raw_args, namespace=Args(**arg_overrides))
 
-    options = set(args.digit_set) if not args.nop else set()
-
+    options = set(args.digit_set[1]) if not args.nop else set()
     sums: defaultdict[int, set[SudokuSet[int]]] = defaultdict(set)
 
     for i in range(1, len(options) + 1):
@@ -317,12 +335,18 @@ def main(prev_alternatives: Alternatives = None, raw_args: list[str] = None, dep
         n = args.num_digits
         tmp = (x for x in tmp if len(x) in n)
 
+    def range_condition(mask, comp) -> bool:
+        (start, end), d = mask
+        start = int(start) if start else (1 if end else len(d))
+        end = int(end) if end else len(d)
+        return start <= comp <= end
+
     if args.include_digits:
-        tmp = (x for x in tmp if any(start <= len(x.intersection(d)) <= end
-                                     for (start, end), d in args.include_digits))
+        tmp = (x for x in tmp if any(range_condition(mask, len(x.intersection(mask[1])))
+                                     for mask in args.include_digits))
     if args.exclude_digits:
-        tmp = (x for x in tmp if all(start <= len(d) - len(x.intersection(d)) <= end
-                                     for (start, end), d in args.exclude_digits))
+        tmp = (x for x in tmp if all(range_condition(mask, len(mask[1]) - len(x.intersection(mask[1])))
+                                     for mask in args.exclude_digits))
 
     if args.num_even:
         tmp = (x for x in tmp if sum(map(lambda y: y % 2 == 0, x)) >= args.num_even)
@@ -332,6 +356,10 @@ def main(prev_alternatives: Alternatives = None, raw_args: list[str] = None, dep
 
     alternatives = list([x] for x in tmp)
 
+    num_columns = len(list(chain.from_iterable((*prev_alternatives[:1], *alternatives[:1]))))
+    if len(args.mask[1]) > num_columns:
+        defer_note(depth_var.get(), f"[b]--mask[/] targeted {len(args.mask[1])} sets, but there are only {num_columns} – maybe look it over.")
+
     if op_func:
         kwargs = {}
         if name := next((k for k, v in op_func.__annotations__.items() if v == Args), None):
@@ -340,19 +368,21 @@ def main(prev_alternatives: Alternatives = None, raw_args: list[str] = None, dep
         if name := next((k for k, v in op_func.__annotations__.items() if v == list[str]), None):
             kwargs[name] = rest
 
-        alternatives = op_func(prev_alternatives, alternatives, **kwargs)
+        alternatives = list(op_func(prev_alternatives, alternatives, **kwargs))
+
         if arg_overrides.get("nop", None):
-            defer_note(depth, f"Nop was set by [b]{op}[/] command. [i]Use --no-nop to override.[/]")
+            defer_note(depth_var.get(), f"Nop was set by [b]{op}[/] command. [i]Use --no-nop to override.[/]")
 
         # if alternatives:
         #     alternatives = list(alternatives)
         #     for a in alternatives:
         #         print(" ".join(map(str, a)))
-
     if rest:
-        alternatives = main(alternatives, rest, depth + 1)
+        reset_token = depth_var.set(depth_var.get() + 1)
+        alternatives = main(alternatives, rest)
+        depth_var.reset(reset_token)
 
-    if depth > 0:
+    if depth_var.get() > 0:
         return alternatives
 
     prepared = sorted((sum(chain(*a)), tuple(map(sum, a)), a) for a in alternatives)
@@ -368,9 +398,12 @@ def main(prev_alternatives: Alternatives = None, raw_args: list[str] = None, dep
         common.intersection_update(digits)
         table.add_row(str(total_sum), str(sep_sum), *map(str, a))
 
-    print(table)
-    print(f"\n  Aggregated digits: {SudokuSet(tuple(seen))}")
-    print(  f"   Digits in common: {SudokuSet(tuple(common))}")
+    if table:
+        print(table)
+        print(f"\n  Aggregated digits: {SudokuSet(tuple(seen))}")
+        print(  f"   Digits in common: {SudokuSet(tuple(common))}")
+    else:
+        print("\n  [dim i]So empty...[/]")
 
     print(f"\n  Number matching results: {len(prepared)}")
 
